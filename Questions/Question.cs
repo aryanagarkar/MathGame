be97e6fbc71d10b73afd438196588c7720330eee
Arya <arya.nagarkar@gmail.com>
2020-08-19 11:53:00 -0700

    Stable class code and unit test committed

---
+++ Questions/Question.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Questions
+{
+    abstract public class Question : MonoBehaviour
+    {
+        protected string difficulty;
+        protected string preferredDisplayType;
+
+        protected int answer;
+        protected string problem;
+
+        string[] possibleAnswers;
+
+
+        public string Difficulty
+        {
+            get { return difficulty; }
+        }
+        public string Problem
+        {
+            get { return problem; }
+        }
+
+        public int Answer
+        {
+            get { return answer; }
+        }
+
+        public string[] PossibleAnswers
+        {
+            get
+            {
+                possibleAnswers = new string[2];
+                int error = 2;
+                possibleAnswers[0] = (answer - error).ToString();
+                possibleAnswers[1] = (answer + error).ToString();
+                return possibleAnswers;
+            }
+        }
+
+        protected abstract void createQuestion();
+
+        public void setProblemAndAnswer()
+        {
+            createQuestion();
+        }
+    }
+}
---
+++ Tests/AdditionQuestionTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Questions;
+
+namespace Tests
+{
+    public class AdditionQuestionTest
+    {
+        [Test]
+        public void createQuestionTest()
+        {
+            AdditionQuestion question = new AdditionQuestion("easy", "circle");
+            question.setProblemAndAnswer();
+            string problem = question.Problem;
+
+            int blankIndex = problem.IndexOf(" ");
+            int secondBlankIndex = problem.IndexOf("+", blankIndex); 
+
+            string num1 = problem.Substring(0, blankIndex + 1);
+            string num2 = problem.Substring(secondBlankIndex + 1); 
+
+            Assert.IsTrue(int.Parse(num1) < 20);  
+            Assert.IsTrue(int.Parse(num2) < 20);
+            Assert.AreEqual(problem, num1 + "+" + num2);        
+        }
+    }
+}
