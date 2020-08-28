0b5268b7740da23a891e4047348034f8e35ed542
Arya <arya.nagarkar@gmail.com>
2020-08-28 13:45:30 -0700

    State folder added

---
+++ State/State.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Questions;
+
+[System.Serializable]
+public class State
+{
+    Grade grade;
+    Journey journey;
+    Topic topic;
+    int difficulty;
+
+    public State(Grade grade, Journey journey, Topic topic, int difficulty){
+        this.grade = grade;
+        this.journey = journey;
+        this.topic = topic;
+        this.difficulty = difficulty;
+    }
+
+    public Grade Grade{
+        get{return grade; }
+        set{value = grade; }
+    }
+
+    public Journey Journey{
+        get{return journey; }
+        set{ value = journey; }
+    }
+
+    public Topic Topic{
+        get{return topic; }
+        set{value = topic; }
+    }
+
+    public int Difficulty{
+        get{return difficulty; }
+        set{value = difficulty; }
+    }
+
+    public bool isEqual(State other)
+    {
+        if(grade == other.Grade && journey == other.Journey
+         && topic == other.topic && difficulty == other.Difficulty){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ State/StateMachine.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Questions;
+using events;
+
+public class StateMachine
+{
+    State state;
+    History hist;
+
+    public StateMachine()
+    {
+        ArrayList topics = new ArrayList();
+        Topic topic1 = new Topic(5, "addition");
+        Topic topic2 = new Topic(5, "subtraction");
+        topics.Add(topic1);
+        topics.Add(topic2);
+        ArrayList journeys = new ArrayList();
+        Journey journey1 = new Journey(topics);
+        journeys.Add(journey1);
+        Grade grade1 = new Grade(journeys);
+
+        state = new State(grade1, journey1, topic1, 5);
+        hist = new History();
+        hist.push(state);
+    }
+
+    public State CurrentState
+    {
+        get { return state; }
+        set { state = value; }
+    }
+
+    public History CurrentHistory{
+        get { return hist; }
+        set { hist = value; }
+    }
+
+    public State getNextState(EventTypes ev)
+    {
+        if (ev == EventTypes.rightAnswerEvent)
+        {
+            return getNextLevelForRightAnswer();
+        }
+        if (ev == EventTypes.wrongAnswerEvent)
+        {
+            return getNextLevelForWrongAnswer();
+        }
+        return null;
+    }
+
+    private State getNextLevelForRightAnswer()
+    {
+        int difficulty = state.Difficulty;
+        int maxDifficulty = state.Topic.MaxDifficulty;
+
+        if (difficulty < maxDifficulty)
+        {
+            State newState = new State(state.Grade, state.Journey, state.Topic, difficulty + 1);
+            state = newState;
+            hist.push(newState);
+            return newState;
+        }
+        else
+        {
+            return state;
+        }
+    }
+
+    private State getNextLevelForWrongAnswer()
+    {
+        int difficulty = state.Difficulty;
+        int maxDifficulty = state.Topic.MaxDifficulty;
+
+        if (difficulty != 1)
+        {
+            State newState = new State(state.Grade, state.Journey, state.Topic, difficulty - 1);
+            state = newState;
+            hist.push(newState);
+            return newState;
+        }
+        else
+        {
+            return state;
+        }
+    }
+}
